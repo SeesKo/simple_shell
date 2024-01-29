4067d93d4a7929ab554252811c6f0c7905753397
Siskosee <seeskosee@seeskoseesko.com>
2024-01-29 12:55:32 -0800

    main header

---
+++ shell.h
@@ -0,0 +1,29 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* STANDARD LIBRARY HEADERS */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/* ENVIRONMENT VARIABLES */
+extern char **environ;
+
+/* USER-DEFINED FUNCTION PROTOTYPES */
+void prompt_me(void);
+char *read_line(void);
+void exec_command(char *input, int *exit_status);
+void find_executable(char *command, char *tokens[]);
+void execute_command(char *tokens[]);
+void parse_line(char *input, char **tokens, int *token_count);
+void handle_builtin(char *command, int *exit_status);
+void exit_shell(int *exit_status);
+void print_env(void);
+
+#endif /* SHELL_H */
