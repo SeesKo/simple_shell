a251ad1a8eb9dd2191bc09a67da4e5fbe0f882e7
Siskosee <seeskosee@seeskoseesko.com>
2024-03-04 11:46:59 -0800

    main header

--- shell.h
+++ shell.h
@@ -1,31 +1,17 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-/* STANDARD LIBRARY HEADERS */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <stdbool.h>
-#include <errno.h>
-#include <limits.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 
-/* ENVIRONMENT VARIABLES */
-extern char **environ;
+#define BUFFER_SIZE 1024
 
-#define MAX_TOKENS 64
-
-/* USER-DEFINED FUNCTION PROTOTYPES */
-void prompt_me(void);
-char *read_line(void);
-void exec_command(char *input, int *exit_status);
-void find_executable(char *command, char *tokens[]);
-void execute_command(char *tokens[]);
-void parse_line(char *input, char **tokens, int *token_count);
-void handle_builtin(char *command, int *exit_status);
-void exit_shell(int *exit_status);
-void print_env(void);
+void display_prompt(void);
+char *read_user_input(void);
+void execute_command(char *command);
 
 #endif /* SHELL_H */
