ddeaf26a5d1adbc8fe84d3e7917f4a6643aa3b3b
Siskosee <seeskosee@seeskoseesko.com>
2024-03-04 11:46:15 -0800

    program entry

--- main.c
+++ main.c
@@ -1,78 +1,101 @@
 #include "shell.h"
 
 /**
+ * display_prompt - Displays prompt.
+ */
+void display_prompt(void)
+{
+	printf("($) ");
+	fflush(stdout);
+}
+
+/**
+ * read_user_input - Reads a line of input from the user.
+ *
+ * Return: A dynamically allocated string containing
+ * the user input or NULL if an error occurs.
+ */
+char *read_user_input(void)
+{
+	char *buffer = NULL;
+	size_t bufsize = 0;
+
+	if (getline(&buffer, &bufsize, stdin) == -1)
+	{
+		printf("\n");
+		free(buffer);
+		exit(EXIT_SUCCESS);
+	}
+
+	buffer[strlen(buffer) - 1] = '\0';
+
+	return (buffer);
+}
+
+/**
+ * execute_command - Executes a command using the
+ * execve system call.
+ * @command: The command to find the executable path for.
+ */
+void execute_command(char *command)
+{
+	pid_t pid = fork();
+
+	if (pid == -1)
+	{
+		perror("fork");
+		free(command);
+		exit(EXIT_FAILURE);
+	}
+	if (pid == 0)
+	{
+		char **argv = (char **)malloc(2 * sizeof(char *));
+
+		if (argv == NULL)
+		{
+			perror("malloc");
+			free(command);
+			exit(EXIT_FAILURE);
+		}
+		argv[0] = command;
+		argv[1] = NULL;
+		if (execve(command, argv, NULL) == -1)
+		{
+			perror("execve");
+			free(command);
+			free(argv);
+			exit(EXIT_FAILURE);
+		}
+		free(argv);
+	}
+	else
+	{
+		int status;
+
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status) && WEXITSTATUS(status) == 127)
+			fprintf(stderr, "%s: command not found\n", command);
+	}
+}
+
+/**
  * main - Main function for the shell.
  *
  * Return: 0 on success.
  */
 int main(void)
 {
-	char *input;
-	int exit_status = 0;
-	bool is_interactive = isatty(STDIN_FILENO);
+	char *command;
 
-	/* Interactive mode */
-	if (is_interactive)
+	while (1)
 	{
-		while (1)
-		{
-			/* Prompt display only in interactive */
-			prompt_me();
-			/* Reading user input */
-			input = read_line();
-
-			/* Check for End-Of-File(EOF) condition */
-			if (input == NULL)
-			{
-				printf("\n");
-				break;
-			}
-
-			/* Execute the command */
-			handle_builtin(input, &exit_status);
-
-			/* Free the allocated memory */
-			free(input);
-		}
+		display_prompt();
+		command = read_user_input();
+		execute_command(command);
+		free(command);
 	}
-	/* Non-interactive mode */
-	else
-	{
-		input = read_line();
 
-		handle_builtin(input, &exit_status);
-
-		free(input);
-	}
+	free(command);
 
 	return (0);
-}
-
-/**
- * parse_line - Tokenizes input to split command and arguments.
- * @input: The input command string to parse.
- * @tokens: Array to store the parsed tokens.
- * @token_count: Pointer to an integer to store the count of tokens.
- */
-void parse_line(char *input, char **tokens, int *token_count)
-{
-	char *token;
-	*token_count = 0;
-
-	/* Tokenize input to split command and arguments */
-	token = strtok(input, " ");
-	while (token != NULL)
-	{
-		tokens[*token_count] = strdup(token);
-		if (tokens[*token_count] == NULL)
-		{
-			perror("Memory allocation error");
-			exit(EXIT_FAILURE);
-		}
-		(*token_count)++;
-		token = strtok(NULL, " ");
-	}
-
-	/* Null-terminate array */
-	tokens[*token_count] = NULL;
 }
